12b87b9b271ce97c5a708ec4501522cde6950c84
Oliviagiles09 <giles11@usf.edu>
2025-02-02 19:21:28 -0500

    Task 2 - Modifying Customers Orders

--- cc_3.js
+++ cc_3.js
@@ -5,3 +5,10 @@ prices.shift();
 
 consol.log("Updated prices", prices);
 
+// Task 2
+let orders = [200, 300, 400, 500, 600];
+orders[2] += 5;
+let totalOrders = orders.reduce((sum, order) => sum + order, 0);
+
+consol.log("Updated Orders:", orders);
+consol.log("Total Order Count:", totalOrders);
